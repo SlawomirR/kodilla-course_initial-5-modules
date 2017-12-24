c48a15c28cd4baaad1279b6a52578254238ef6b0
SlawomirR <no.for.spam@wp.pl>
2017-12-24 10:19:18 +0100

    Improved code clarity.

--- 3_2_Collection_Framework_-_Queue_interface_and_ArrayDeque_class/src/Main_3_2.java
+++ 3_2_Collection_Framework_-_Queue_interface_and_ArrayDeque_class/src/Main_3_2.java
@@ -1,40 +1,43 @@
 import java.util.ArrayDeque;
 import java.util.Objects;
 
-public class Main_3_2 {
+class Main_3_2 {
     public static void main(String[] args) {
-        int maxBooksNumber = 5;
-        int baseYearForCalculation = 2005;
         ArrayDeque<Book> books = new ArrayDeque<>();
-        System.out.println("We work with: " + books.getClass());
-        System.out.println("What is the size of the queue: " + books.size());
-        System.out.println("Books added to the queue:");
-        // dodanie książek na stos
-        for (int i = 1; i < maxBooksNumber + 1; i++) {
-            books.push(new Book("Book_" + i, "Author_" + i, baseYearForCalculation+maxBooksNumber+1-2*i+2));
-            System.out.println(i + ". Book_" + i + " Author_" + i + " " + (baseYearForCalculation+maxBooksNumber+1-2*i+2));
+        buildNewBookStack(books);
+        processTasksWithBooks(books);
+        buildNewBookStack(books);
+        cancelingTasksWithBooks(books);
+    }
+
+    private static void processTasksWithBooks(ArrayDeque<Book> books) {
+        Book temporaryItem = new Book();
+        while (books.peek() != null) {
+            temporaryItem = books.poll();
+            System.out.println("We are processing " + temporaryItem);
         }
-        System.out.println("We have " + books.size() + " books in our queue");
-        // zdejmowanie kolejnych książek ze stosu
-        while (books.size() > 1) {
-            System.out.println("We are processing " + books.peek());
-            System.out.println("taking out " + books.poll() + " ... done");
-            System.out.println("Left " + books.size() + " books to proceed.");
-        }
-        System.out.print("The last book we can process is: ");
-        System.out.println(books.getLast());
-        System.out.println(" ... so taking out last one: " + books.poll() + " ... done.");
+        System.out.println(temporaryItem.toString() + " was the last book we processed.");
         System.out.println("How many books left? -> " + books.size());
-        System.out.println("New books added to the queue:");
-        // nowe dodanie książek na stos
-        for (int i = 1; i < maxBooksNumber + 1; i++) {
-            books.push(new Book("Book_" + i, "Author_" + i, baseYearForCalculation+maxBooksNumber+1-2*i+2));
-            System.out.println(i + ". Book_" + i + " Author_" + i + " " + (baseYearForCalculation+maxBooksNumber+1-2*i+2));
-        }
-        System.out.println("We have " + books.size() + " books in our queue");
+    }
+
+    private static void cancelingTasksWithBooks(ArrayDeque<Book> books) {
         System.out.println("We clean up all book at once");
         books.clear();
         System.out.println("We have " + books.size() + " books in our queue");
+    }
+
+    private static void buildNewBookStack(ArrayDeque<Book> books) {
+        System.out.println("Books added to the queue:");
+        int maxBooksNumber = 5;
+        for (int i = 1; i < maxBooksNumber + 1; i++) {
+            books.push(new Book("Book_" + i, "Author_" + i, calculateFakeYearOfPublish(i)));
+            System.out.println(i + ". Book_" + i + " Author_" + i + " " + calculateFakeYearOfPublish(i));
+        }
+        System.out.println("We have " + books.size() + " books in our queue");
+    }
+
+    private static int calculateFakeYearOfPublish(int currentBookNumber) {
+        return 2018 - 3*currentBookNumber;
     }
 }
 
@@ -43,10 +46,7 @@ final class Book {
     private String author;
     private int year;
 
-    public int getYear() {
-        return year;
-    }
-
+    Book() {}
     Book(String title, String author, int year) {
         this.title = title;
         this.author = author;
