ba2af046d7f43befefface0cf7979825873c3cc5
SlawomirR <no.for.spam@wp.pl>
2017-12-25 13:07:23 +0100

    3_4_For_and_While_loop_in_collection - initial commit.

---
+++ 3_4_Examples_of_using_For_and_While_loops_in_collections/src/Main_3_4.java
@@ -0,0 +1,13 @@
+public class Main_3_4 {
+    /*
+    Zadanie składa się z dwóch części:
+    Część 1
+    1. Napisz program, który w kolejce ArrayDeque umieści 50 obiektów przechowujących napisy,
+    składające się z litery 'a' powtórzonej losową ilość razy (zakres powtórzeń: 1 - 50).
+    2. Wypełnienie obiektu powtórzeniami litery 'a' zrealizuj przy pomocy pętli for.
+    Część 2
+    1. Program z części pierwszej rozbuduj w taki sposób, że przekażesz utworzoną kolejkę do
+    metody Twojej klasy, która dokona rozdzielenia obiektów z kolejki na dwie kolekcje ArrayList.
+    2. Jedna z nich będzie przechowywała obiekty o parzystej ilości znaków 'a', a druga o nieparzystej.
+    */
+}
