449e8b20ef86a5b6934c088846eb4f2ddbb9d795
SlawomirR <no.for.spam@wp.pl>
2017-12-26 00:02:47 +0100

    FindTheFrequencyOfTheLettersOccurrence - initial commit.

---
+++ FindTheFrequencyOfTheLettersOccurrence/src/FindTheFrequencyOfTheLettersOccurrence.java
@@ -0,0 +1,10 @@
+public class FindTheFrequencyOfTheLettersOccurrence {
+    /*
+    Napisz program zliczajacy ile razy w wyrazie występuje dana litera, np:
+
+    podajesz wyraz - programowanie
+    podajesz litere - r
+    rezultat: 2
+    */
+
+}
