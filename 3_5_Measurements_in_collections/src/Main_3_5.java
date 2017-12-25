5c701836f94cae2d8970902f00951bc851b8cd9e
SlawomirR <no.for.spam@wp.pl>
2017-12-25 23:22:45 +0100

    3_5_Measurements_in_collections - initial commit.

---
+++ 3_5_Measurements_in_collections/src/Main_3_5.java
@@ -0,0 +1,14 @@
+public class Main_3_5 {
+    /*
+    Zadanie składa się z dwóch części.
+    Część 1 dla LinkedList
+    Stwórz klasę reprezentującą książkę o nazwie Book. Klasa powinna mieć dwa pola: author oraz title.
+    Pamiętaj o implementacji metod hashCode() oraz equals(Object o). Będziemy jej używali jako obiektów kolekcji LinkedList.
+    Stwórz program, który zmierzy czas operacji wyszukiwania i usunięcia obiektu na początku
+    (z użyciem metody remove(Object o)), wyszukiwania i usunięcia obiektu na końcu (z użyciem metody remove(Object o)),
+    wstawiania na początku oraz wstawiania na końcu listy LinkedList liczącej kilka milionów obiektów.
+    Część 2 dla HashMap
+    Stwórz program, który zmierzy czas operacji wyszukiwania po kluczu, a także czas dodawania
+    i usuwania obiektu z mapy HashMap liczącej kilka milionów obiektów.
+    */
+}
