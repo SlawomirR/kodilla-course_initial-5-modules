789abb53f4bbaf6ba98ebaea53019c0e67a943d4
SlawomirR <no.for.spam@wp.pl>
2017-12-27 09:36:07 +0100

    Kalkulator - the first working version with very limited functionality.

--- Kalkulator/src/Calculator.java
+++ Kalkulator/src/Calculator.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class Calculator {
+    private static final String PLUS = "+";
+    private static final String MINUS = "-";
+    public static void main(String[] args) {
+        double numberA, numberB;
+        String sign;
+        Calculator calculator = new Calculator();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Podaj pierwszą liczbę:");
+        numberA = input.nextInt();
+        System.out.print("Podaj drugą liczbę:");
+        numberB = input.nextInt();input.nextLine();
+        System.out.print("Podaj znak działania: + lub - Jaki wybór? ");
+        sign = input.nextLine();
+        switch (sign) {
+            case PLUS:
+                calculator.addition(numberA, numberB);
+                break;
+            case MINUS:
+                calculator.subtraction(numberA, numberB);
+                break;
+            default:
+                System.err.println("WARNING: Only sign of \"+\" or \"-\" are allowed to use!");
+        }
+    }
+
+    private void addition(double numberA, double numberB) {
+        System.out.println("Wynik dodawania: " + (numberA+numberB));
+    }
+
+    private void subtraction(double numberA, double numberB) {
+        System.out.println("Wynik odejmowania: " + (numberA-numberB));
+    }
 }
+
