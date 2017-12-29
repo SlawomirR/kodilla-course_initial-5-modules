a349da5097b5e04dba4359bcd08f470aed592186
SlawomirR <no.for.spam@wp.pl>
2017-12-29 21:07:46 +0100

    find.sh - useful script help finding information of dangling objects

---
+++ find.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Usage: ./find.sh dangling_SHA1_number
+obj_name="$1"
+shift
+git log "$@" --pretty=format:'%T %h %s' \
+| while read tree commit subject ; do
+    if git ls-tree -r $tree | grep -q "$obj_name" ; then
+        echo $commit "$subject"
+    fi
+done
