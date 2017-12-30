e80a6c05158f9524f31a8790f4af247e25ebf000
SlawomirR <no.for.spam@wp.pl>
2017-12-30 01:13:19 +0100

    FindTheFrequencyOfTheLettersOccurrence - the first working version.

--- FindTheFrequencyOfTheLettersOccurrence/src/FindTheFrequencyOfTheLettersOccurrence.java
+++ FindTheFrequencyOfTheLettersOccurrence/src/FindTheFrequencyOfTheLettersOccurrence.java
@@ -1,10 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+/*
+Napisz program zliczajacy ile razy w wyrazie występuje dana litera, np:
+podajesz wyraz - programowanie
+podajesz litere - r
+rezultat: 2
+*/
 public class FindTheFrequencyOfTheLettersOccurrence {
-    /*
-    Napisz program zliczajacy ile razy w wyrazie występuje dana litera, np:
-
-    podajesz wyraz - programowanie
-    podajesz litere - r
-    rezultat: 2
-    */
-
+    public static void main(String[] args) {
+        String sampleText = "Programowanie jest wspaniałym zajęciem dla programistów lubiących programować i tworzyć programy dla innych";
+        ArrayList<String> sss = new ArrayList<>();
+        for (int i = 0; i< sampleText.length(); i++) {
+            sss.add(String.valueOf(sampleText.charAt(i)));
+        }
+        System.out.println(Collections.frequency(sss, "r"));
+    }
 }
