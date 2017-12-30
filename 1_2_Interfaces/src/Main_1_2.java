4b843e37c2496fe2986fef2746d838e91578011f
SlawomirR <no.for.spam@wp.pl>
2017-12-30 15:36:06 +0100

    1_2 - updated task description.

--- 1_2_Interfaces/src/Main_1_2.java
+++ 1_2_Interfaces/src/Main_1_2.java
@@ -1,3 +1,13 @@
+/*
+Stwórz interfejs obsługujący bankomat (eng. ATM) zawierający:
+
+    Dwie metody wymagające implementacji - wpłata i wypłata
+    Metodę domyślną - informacja o połączeniu z bankiem
+    Metodę statyczną - informacja o zakończeniu działania bankomatu
+
+Następnie zaimplementuj taki interface do klasy o nazwie ATMImpl (ATM Implementation) i stwórz
+ciała dla metod, które tego wymagają. W klasie Application w metodzie main wywołaj każdą metodę.
+*/
 public class Main_1_2 { // Bankomat version 1.0
         public static void main(String[] args) {
             ATMImpl atm = new ATMImpl();
