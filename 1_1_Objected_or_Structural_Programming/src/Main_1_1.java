0f1bb303a77a0689af0aa44781c94555fc6871d2
SlawomirR <no.for.spam@wp.pl>
2017-12-30 15:33:40 +0100

    1_1 - updated task description.

--- 1_1_Objected_or_Structural_Programming/src/Main_1_1.java
+++ 1_1_Objected_or_Structural_Programming/src/Main_1_1.java
@@ -1,3 +1,27 @@
+/*
+Przeprogramować poniższy kod według podejścia obiektowego:
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Application
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String name = "Adam";
+		double age = 40.5;
+		double height = 178;
+
+		if(name != null) {
+			if(age > 30 && height > 160) {
+				System.out.println("User is older than 30 and higher then 160cm");
+			} else {
+				System.out.println("User is younger than 30 or lower than 160cm");
+			}
+		}
+	}
+}
+*/
 public class Main_1_1 {
     public static void main(String[] args) {
         // zmienne do kontroli granic wieku i wzrostu uzytkownika
