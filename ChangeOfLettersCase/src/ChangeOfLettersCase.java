70a024f558868f762012c4fd3a2878691726c614
SlawomirR <no.for.spam@wp.pl>
2017-12-30 15:19:37 +0100

    ChangeOfLettersCase - the first working version.

--- ChangeOfLettersCase/src/ChangeOfLettersCase.java
+++ ChangeOfLettersCase/src/ChangeOfLettersCase.java
@@ -1,8 +1,24 @@
-public class ChangeOfLettersCase {
-    /*
-    Napisz program odwracający rozmiar liter w słowie
-
+/*
+Napisz program odwracający rozmiar liter w słowie
 podajesz : AbC
-rezultat: aBc
-     */
+rezultat : aBc
+*/
+public class ChangeOfLettersCase {
+    public static void main(String[] args) {
+        String sampleString = "To JesT prZykłADowY tExT";
+
+        StringBuilder stringBuilder = new StringBuilder();
+        int stringLength = sampleString.length();
+
+        System.out.println("String example:\n" + sampleString);
+        char[] charArray = sampleString.toCharArray();
+        for (int i = 0; i < stringLength; i++) {
+            if (Character.isLowerCase(charArray[i])) {
+                stringBuilder.append(Character.toUpperCase(charArray[i]));
+            } else {
+                stringBuilder.append(Character.toLowerCase(charArray[i]));
+            }
+        }
+        System.out.print("Reversed case of letters:\n" + stringBuilder);
+    }
 }
