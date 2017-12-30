a24fee581bb62e73a90e5b216eb1c9bcd7d92004
SlawomirR <no.for.spam@wp.pl>
2017-12-30 15:28:26 +0100

    0_6 task description.

--- 0_6_Introduction_to_Java/src/Main_0_6.java
+++ 0_6_Introduction_to_Java/src/Main_0_6.java
@@ -1,3 +1,28 @@
+/*
+KROK 1 Stwórz klasę
+
+    Nazwij ją User.
+    Dodaj atrybuty użytkownika takie jak: imię, nazwisko, płeć, wiek, mail.
+    Utwórz konstruktor dla tej klasy.
+    Stwórz metody charakterystyczne dla klasy użytkownika tj.: logowanie, usunięcie konta.
+    Stwórz gettery dla atrybutów klasy.
+
+KROK 2 Stwórz klasę podrzędną (dziedziczącą po klasie User).
+
+    Nazwij ją ForumUser.
+    Dodaj atrybuty charakterystyczne dla użytkownika forum np. nazwa użytkownika, liczba postów, zalogowany (true/false).
+    Utwórz konstruktor dla użytkownika forum.
+    Stwórz metody charakterystyczne dla użytkownika forum np. opublikowanie postu na forum czy komentowanie postu oraz taką która będzie przedstawiała użytkownika w konsoli.
+    Odziedzicz właściwości klasy User.
+    Rozbuduj konstruktor o metodę super().
+
+KROK 3 Stwórz klasę Application:
+
+    Stwórz w niej metodę main().
+    Utwórz nowy obiekt ForumUser.
+    Wywołaj metodą która przedstawi nam użytkownika forum w konsoli.
+
+*/
 public class Main_0_6 {
     public static void main(String[] args) {
         ForumUser forumUser = new ForumUser("Jan", "Kowalski", "male", 25, "jan.kowalski@kodilla.com",
