dd99254100c2feb9c90697ed90e22af35e9f8958
SlawomirR <no.for.spam@wp.pl>
2017-12-31 18:24:06 +0100

    1_4_Abstract_Class - updated task info.

--- 1_4_Abstract_Class/src/Main_1_4.java
+++ 1_4_Abstract_Class/src/Main_1_4.java
@@ -1,3 +1,19 @@
+/*
+Zadanie: Transakcja bankowa
+
+Aby przećwiczyć wykorzystywanie klas abstrakcyjnych w praktyce, stwórz swoje własne zachowanie dla transakcji bankowej.
+Załóżmy, że transakcja bankowa zawiera się w 8 metodach, z czego 3 są zawsze po stronie banku (w klasie abstrakcyjnej).
+Schemat transakcji bankowej do zaimplementowania:
+    Prośba o wprowadzenie karty (Bankomat)
+    Prośba o wprowadzenie kodu PIN (Bankomat)
+    Prośba o wprowadzenie kwoty do wypłaty (Bankomat)
+    Informacja o przyjęciu prośby wypłaty przez Bank (Bank)
+    Sprawdzenie stanu konta (Bank)
+    Potwierdzenie możliwości wypłaty (Bank)
+    Wypłacanie gotówki (Bankomat)
+    Podziękowanie za skorzystanie z usług Banku (Bankomat)
+Załóż, że każde z tych działań sygnalizujemy zwykłym println.
+*/
 public class Main_1_4 {
     public static void main(String[] args) {
         TransactionRunner makeTransaction = new TransactionRunner();
