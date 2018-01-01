be90df24c8a7854e21e36284a58b558c88c21f94
SlawomirR <no.for.spam@wp.pl>
2018-01-01 22:19:06 +0100

    2_6_Map_And_HashMap - updated content of the task.

--- 2_6_Map_And_HashMap/src/Main_2_6.java
+++ 2_6_Map_And_HashMap/src/Main_2_6.java
@@ -1,5 +1,7 @@
 /*
-
+Zadanie: mapa uczniów i ocen
+    Stwórz program z użyciem HashMap, który będzie przechowywał dane uczniów oraz ich ocen.
+    Program ma wyświetlać średnią arytmetyczną ocen każdego ucznia.
 */
 public class Main_2_6 {
 }
